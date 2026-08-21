760c72e346c5318d0aca9bc02c15c6c2a5acfabe
Leonardo <leonardo.barbosaper@gmail.com>
2026-08-21 18:23:32 -0300

    fix(database): atualiza classe Database para carregar configurações do arquivo .env

--- app/config/Database.php
+++ app/config/Database.php
@@ -1,12 +1,30 @@
 <?php
 
 class Database{
-    private $host = 'localhost';
-    private $db_name = 'oktano';
-    private $username = 'root';
-    private $password = '';
-    private $port = '3307';
+    private $host;
+    private $port;
+    private $db_name;
+    private $username;
+    private $password;
     private $conn;
+
+    public function __construct(){
+        $caminhoEnv = __DIR__ . '/../../.env';
+
+        if(file_exists($caminhoEnv)){
+            $env = parse_ini_file($caminhoEnv); //le e transforma em array
+
+            $this->host = $env['DB_HOST'];
+            $this->port = $env['DB_PORT'];
+            $this->db_name = $env['DB_NAME'];
+            $this->username = $env['DB_USER'];
+            $this->password = $env['DB_PASS'];
+        } else{
+            error_log('Erro Crítico: Arquivo de configuração .env não encontrado no caminho: ' . $caminhoEnv);
+            
+            die("O sistema está passando por uma manutenção temporária. Por favor, tente novamente mais tarde.");
+        }
+    }
 
     public function conectar() {
         $this->conn = null;
