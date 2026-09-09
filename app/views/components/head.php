9a5957abedee0a3819eab662009c4eb6dcc4124a
skibadominic <skibadominic@gmail.com>
2026-09-09 15:35:21 -0300

    style: logo adicionada ao favico

--- app/views/components/head.php
+++ app/views/components/head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $tituloPagina ?? "Oktano" ?></title>
+    <link rel="icon" type="image/svg+xml" href="/oktano/public/assets/img/sem%20texto1.svg">
 
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
 
