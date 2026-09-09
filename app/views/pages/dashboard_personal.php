654caab586a4a2c2012ab7206600a369e915e8b8
Luan Fafel <luanfafel@gmail.com>
2026-09-09 11:41:42 -0300

    feat(DRY): reutiliza component head no dashboard do personal

--- app/views/pages/dashboard_personal.php
+++ app/views/pages/dashboard_personal.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Personal - Oktano</title>
-
-    <script src="https://unpkg.com/@phosphor-icons/web"></script>
-
-    <link rel="stylesheet" href="/oktano/public/assets/css/style.css">
-    <link rel="stylesheet" href="/oktano/public/assets/css/header.css">
-</head>
+<?php
+$tituloPagina = "Dashboard Personal - Oktano";
+$estilosCSS = ['header'];
+require_once __DIR__ . '/../components/head.php';
+?>
 <body>
     <?php
         require_once __DIR__ . '/../components/header_personal.php';
